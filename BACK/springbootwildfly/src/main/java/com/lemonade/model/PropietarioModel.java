c02d389e703a717729abb35cc878ee448be8eb5f
jose lozano <jose.lozano@smartsoft.com.co>
2019-01-26 17:00:07 -0500

    implementacion de jpa en spring

---
+++ BACK/springbootwildfly/src/main/java/com/lemonade/model/PropietarioModel.java
@@ -0,0 +1,68 @@
+package com.lemonade.model;
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "propietario")
+public class PropietarioModel implements Serializable{
+	
+	@Id
+	private String cedula ;
+	
+	@Column
+	private String  nombre;
+	
+	@Column
+	private String  sexo;
+	
+	@Column
+	private String indicativo;
+	
+	@Column
+	private String telefono ;
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getIndicativo() {
+		return indicativo;
+	}
+
+	public void setIndicativo(String indicativo) {
+		this.indicativo = indicativo;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+}
